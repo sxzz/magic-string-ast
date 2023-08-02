2601d472da498cd6c9a704ba71ad5c4a4411e915
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-08-03 01:24:35 +0800

    chore: remove postbuild script

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   entry: ['./src/index.ts'],
   format: ['cjs', 'esm'],
   target: 'node16.14',
+  splitting: true,
+  cjsInterop: true,
   clean: true,
   dts: true,
 })
